470313267924a354b707b8a0c66ad872daa8a714
SoufianeNajim <soufianenajim100@gmail.com>
2024-04-15 14:31:15 +0100

    DP-prog

--- ProjectPro_B/app/Providers/AppServiceProvider.php
+++ ProjectPro_B/app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Repositories\AdminRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +12,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(AdminRepository::class, function ($app) {
+            return new AdminRepository();
+        });
+
     }
 
     /**
---
+++ ProjectPro_B/app/RepositoryInterfaces/UserRepositoryInterface.php
@@ -0,0 +1,9 @@
+<?php
+
+
+namespace App\RepositoryInterfaces;
+
+interface UsersRepositoryInterface
+{
+
+}
