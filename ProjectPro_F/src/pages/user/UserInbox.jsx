7e83fb8294423d4fe4253ad1017b886b27b85a23
SoufianeNajim <soufianenajim100@gmail.com>
2024-04-22 00:30:02 +0100

    notification ui

--- ProjectPro_F/src/pages/user/UserInbox.jsx
+++ ProjectPro_F/src/pages/user/UserInbox.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Chip } from "@nextui-org/react";
+import { Avatar, Chip } from "@nextui-org/react";
 import { Button } from "@material-tailwind/react";
 
 import {
@@ -23,27 +23,130 @@ const UserInbox = () => {
       <div>
         <div className="">
           <Card className="mt-6 w-11/12 flex">
-            <CardBody className=" flex gap-5 items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="mb-4 h-12 w-12 text-gray-900"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M9.315 7.584C12.195 3.883 16.695 1.5 21.75 1.5a.75.75 0 01.75.75c0 5.056-2.383 9.555-6.084 12.436A6.75 6.75 0 019.75 22.5a.75.75 0 01-.75-.75v-4.131A15.838 15.838 0 016.382 15H2.25a.75.75 0 01-.75-.75 6.75 6.75 0 017.815-6.666zM15 6.75a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5z"
-                  clipRule="evenodd"
+            <CardBody className=" flex gap-5 items-center justify-between">
+              <div className="flex gap-4 items-center">
+                <Avatar
+                  isBordered
+                  src="https://i.pravatar.cc/150?u=a042581f4e29026024d"
                 />
-                <path d="M5.26 17.242a.75.75 0 10-.897-1.203 5.243 5.243 0 00-2.05 5.022.75.75 0 00.625.627 5.243 5.243 0 005.022-2.051.75.75 0 10-1.202-.897 3.744 3.744 0 01-3.008 1.51c0-1.23.592-2.323 1.51-3.008z" />
-              </svg>
-              <Typography variant="h5" color="blue-gray" className="mb-2">
-                UI/UX Review Check
-              </Typography>
-              <Typography>
-                Because it&apos;s about motivating the doers. Because I&apos;m
-                here to follow my dreams and inspire others.
-              </Typography>
+                <div>
+                  <Typography variant="h5" color="blue-gray" className="">
+                    Marco
+                  </Typography>
+                  <Typography>
+                    Invited you to join his Project teslabuild
+                  </Typography>
+                </div>
+              </div>
+
+              <div className="flex items-center gap-4">
+                <Button color="red" className="rounded-full">
+                  Refusez
+                </Button>
+                <Button color="green" className="rounded-full">
+                  Accpeter
+                </Button>
+              </div>
+            </CardBody>
+          </Card>
+          <Card className="mt-6 w-11/12 flex">
+            <CardBody className=" flex gap-5 items-center justify-between">
+              <div className="flex gap-4 items-center">
+                <Avatar
+                  isBordered
+                  src="https://i.pravatar.cc/150?u=a042581f4e29026024d"
+                />
+                <div>
+                  <Typography variant="h5" color="blue-gray" className="">
+                    Marco
+                  </Typography>
+                  <Typography>
+                    Invited you to join his Project teslabuild
+                  </Typography>
+                </div>
+              </div>
+
+              <div className="flex items-center gap-4">
+                <Button color="red" className="rounded-full">
+                  Refusez
+                </Button>
+                <Button color="green" className="rounded-full">
+                  Accpeter
+                </Button>
+              </div>
+            </CardBody>
+          </Card>
+          <Card className="mt-6 w-11/12 flex">
+            <CardBody className=" flex gap-5 items-center justify-between">
+              <div className="flex gap-4 items-center">
+                <Avatar
+                  isBordered
+                  src="https://i.pravatar.cc/150?u=a042581f4e29026024d"
+                />
+                <div>
+                  <Typography variant="h5" color="blue-gray" className="">
+                    Marco
+                  </Typography>
+                  <Typography>
+                    Invited you to join his Project teslabuild
+                  </Typography>
+                </div>
+              </div>
+
+              <div className="flex items-center gap-4">
+                <Button color="red" className="rounded-full">
+                  Refusez
+                </Button>
+                <Button color="green" className="rounded-full">
+                  Accpeter
+                </Button>
+              </div>
+            </CardBody>
+          </Card>
+          <Card className="mt-6 w-11/12 flex">
+            <CardBody className=" flex gap-5 items-center justify-between">
+              <div className="flex gap-4 items-center">
+                <Avatar
+                  isBordered
+                  src="https://i.pravatar.cc/150?u=a042581f4e29026024d"
+                />
+                <div>
+                  <Typography variant="h5" color="blue-gray" className="">
+                    Marco
+                  </Typography>
+                  <Typography>
+                    Invited you to join his Project teslabuild
+                  </Typography>
+                </div>
+              </div>
+
+              <div className="flex items-center gap-4">
+                <Button color="red" className="rounded-full">
+                  Refusez
+                </Button>
+                <Button color="green" className="rounded-full">
+                  Accpeter
+                </Button>
+              </div>
+            </CardBody>
+          </Card>
+          <Card className="mt-6 w-11/12 flex">
+            <CardBody className=" flex gap-5 items-center justify-between">
+              <div className="flex gap-4 items-center">
+                <Avatar
+                  isBordered
+                  src="https://i.pravatar.cc/150?u=a042581f4e29026024d"
+                />
+                <div>
+                  <Typography variant="h5" color="blue-gray" className="">
+                    Marco
+                  </Typography>
+                  <Typography>
+                    Invited you to join his Project teslabuild
+                  </Typography>
+                </div>
+              </div>
+
               <div className="flex items-center gap-4">
                 <Button color="red" className="rounded-full">
                   Refusez
