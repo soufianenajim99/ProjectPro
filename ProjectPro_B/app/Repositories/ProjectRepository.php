e4115f0c44c749f8e10fad9ace9c075204800674
SoufianeNajim <soufianenajim100@gmail.com>
2024-04-22 00:22:46 +0100

    adding project leader

--- ProjectPro_B/app/Repositories/ProjectRepository.php
+++ ProjectPro_B/app/Repositories/ProjectRepository.php
@@ -4,14 +4,27 @@ namespace App\Repositories;
 
 use App\Models\Inventory;
 use App\Models\Project;
+use App\Models\ProjectUtilisateur;
 use App\RepositoryInterfaces\ProductbacklogRepositoryInterface;
 use App\RepositoryInterfaces\ProjectRepositoryInterface;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProjectRepository implements ProjectRepositoryInterface
 {
     public function storeproject(array $data){
         $project = Project::create($data);
+
+
+        $scrum_id =Auth::guard('api')->user()->utilisateur()->first()->id;
+
+        $data = [
+            'utilisateur_id' => $scrum_id,
+            'project_id' => $project->id,
+            'role' => "scrum master",
+        ];
+        $pu = ProjectUtilisateur::create($data);
+        
         return response()->json([
             'project_id' => $project->id,
         ]);
